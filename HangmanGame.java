0305fa01db9c5c141c5aa33e4fc51e80de54659e
Patrick Henry <PJWHenry@Patricks-MacBook-Pro.local>
2018-04-10 10:21:32 -0400

    The initial files for the game

---
+++ HangmanGame.java
@@ -0,0 +1,55 @@
+package hangman;
+
+public class HangmanGame implements Game {
+
+	
+	public void initializeProgram() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void greetings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void initializeGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	public char whoMovesFirst() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean computerMove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean userMove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	
+	public boolean playAgain() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void endProgram() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
